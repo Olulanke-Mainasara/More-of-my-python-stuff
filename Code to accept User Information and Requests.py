a03b082dd4c7950b34e11827b6dd6fbc31641bda
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-01-05 20:38:49 +0100

    Updated code

--- Code to accept User Information and Requests.py
+++ Code to accept User Information and Requests.py
@@ -1,3 +1,15 @@
+def main():
+    print("WELCOME")
+    get_request()
+    print("GOODBYE")
+
+def get_request():
+    request = input("Enter your request:")
+    print(request)
+    print("is that your last request?")
+
+main()
+
 name = input("Enter your name: ")
 address = input("Enter your address: ")
 
