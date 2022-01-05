1bf15de59347d3f10742f4c7e50150d37e9ffb19
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-01-05 21:07:31 +0100

    Updated code

--- Another dict kini.py
+++ Another dict kini.py
@@ -1,3 +1,4 @@
+# Code 1
 def main():
     mydict = {}
     userinput = int(input("Enter the length of your dictionary: "))
@@ -10,6 +11,30 @@ def main():
     print("This is post formatting of the phonebook: ")
     mydict.clear()
     print(mydict)
+
+
+main()
+
+# Code 2
+def main():
+    inventory = {"Noreos":20, "Bread":10, "Biscuit":50, "Candy":100, "Food":20}
+    print(inventory.keys())
+    acquire = input("Which product do you want to buy: ")
+    amount = int(input("What quantity of this product do you want: "))
+    if acquire not in inventory:
+        print("Sorry, we don't have this product.")
+    elif inventory[acquire] == 0:
+        print("Sorry, we are out of stock but please do come bck to check again later.")
+    elif amount > inventory[acquire]:
+        print("Sorry, we don't have enough for your purchase.")
+    else:
+        inventory[acquire] = inventory[acquire] = amount
+        print("Good choice.")
+
+    print(inventory)
+    remove = input("Which item do you want to remove: ")
+    print(inventory.pop(remove))
+    print(inventory)
 
 
 main()
