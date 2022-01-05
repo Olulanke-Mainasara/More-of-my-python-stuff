4e7159555b65adc128a6a1794f2aa949e6429577
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-01-05 20:36:56 +0100

    Update to Repositiory

--- Counting Methods and Operations.py
+++ Counting Methods and Operations.py
@@ -24,3 +24,12 @@ listD = theOGlist[0 : 3]
 listE = theOGlist[3 : 8]
 print(listD)
 print(listE)
+
+# Basic Arithmetics
+def add(a,b):
+    sum = a + b
+    print(sum)
+
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second numbers: "))
+add(num_1, num_2)
