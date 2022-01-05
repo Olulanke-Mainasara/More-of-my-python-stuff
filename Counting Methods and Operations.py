30220b508bda0b1f9030b0a162f14bf3ce08c4ca
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-01-05 21:02:42 +0100

    Updated code

--- Counting Methods and Operations.py
+++ Counting Methods and Operations.py
@@ -1,11 +1,11 @@
-# Code for Counting the  number of figures between a given range
+# Code 1
 mylist = list(range(3, 10))
 count = 0
 for I in mylist:
     count += 1
 print(count)
 
-# Code to check if the number entered by the user is greater than the size of the array of numbers giving
+# Code 2
 mylist = [0,1,3,7,9]
 indexval = int(input("index val: "))
 sizemylist = len(mylist)
@@ -15,7 +15,7 @@ if sizemylist <= indexval:
 else:
     print("correct")
 
-# Code to show the concatenation of two lists
+# Code 3
 listA = list(range(2, 5))
 listB = list(range(5, 10))
 theOGlist = listA + listB
@@ -25,7 +25,7 @@ listE = theOGlist[3 : 8]
 print(listD)
 print(listE)
 
-# Basic Arithmetics
+# Code 4
 def add(a,b):
     sum = a + b
     print(sum)
@@ -34,7 +34,7 @@ num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second numbers: "))
 add(num_1, num_2)
 
-# Code to Edit the Value already given by the Programmer
+# Code 5
 def main():
     value = 99
     print("The value is",value)
@@ -48,7 +48,7 @@ def change_me(arg):
 
 main()
 
-# Code for some normal number operations
+# Code 6
 def main():
     num_1 = int(input("Enter the first number: "))
     num_2 = int(input("Enter the second number: "))
@@ -62,5 +62,23 @@ def compute(first, second, third, fourth, fifth):
     multiplication = first * second * third * fourth * fifth
     return addition, multiplication
 
+
+main()
+
+# Code 7
+def main():
+    Body()
+
+def Body():
+    print("WELCOME")
+    addition()
+    print("Farewell ˆ_ˆ")
+
+def addition():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    sum = num1 + num2
+    print("The sum is ", sum)
+    print("Anything else?")
 
 main()
