20a2d91eec8d0fed61d5706401a1655a74f5512c
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-01-05 20:40:43 +0100

    Updated code

--- Not Qualified/CSC Exercise 2.py
+++ Not Qualified/CSC Exercise 2.py
@@ -1,17 +1,3 @@
-def main():
-    print("WELCOME")
-    get_request()
-    print("GOODBYE")
-
-def get_request():
-    request = input("Enter your request:")
-    print(request)
-    print("is that your last request?")
-
-main()
-
-
-# Code to accept user Information
 name = input("Enter your name: ")
 address = input("Enter your address: ")
 
