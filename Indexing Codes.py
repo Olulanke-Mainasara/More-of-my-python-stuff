f0a51c2ca597b2962452b5e3fc167dd8a7595a58
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-01-05 20:59:18 +0100

    Updated code

--- Indexing Codes.py
+++ Indexing Codes.py
@@ -1,16 +1,18 @@
+# Code 1
 even_numbers = [2,4,6,8,10]
 print(even_numbers)
 for item in even_numbers:
     print(item)
 print(even_numbers[1])
 
+# Code 2
 names = ["Roman", "Phantom", "Jago", "Eight", "Grimlock"]
 print(names)
 for N in names:
     print(N)
-
 print(names[0])
 
+# Code 3
 info = ["Alicia", 27, 1550.87]
 print(info)
 for I in info:
@@ -19,7 +21,7 @@ for I in info:
     else:
         print("Item not found")
 
-
+# Code 4
 numbers = list(range(5))
 print(numbers)
 numbers2 = list(range(2, 16))
